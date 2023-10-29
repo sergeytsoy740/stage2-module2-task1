ad56917235eac9dfbfd43456d8b4c37cb1296caa
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-10-29 17:52:40 +0600

    Module #2. Task #1. Servlets

--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,28 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
+
+@WebServlet("/users")
+public class GetUsersServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        Set<User> userSet = Warehouse.getInstance().getUsers();
+        req.setAttribute("users", userSet);
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("jsp/users.jsp");
+        dispatcher.forward(req, resp);
+
+
+    }
 }
